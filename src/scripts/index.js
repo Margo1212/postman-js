a94ad580d6e7a1db6ccbd9036e51b19a70d61fa6
Ewelina W <ewelina.wystepek@gmail.com>
2021-12-28 20:00:58 +0100

    update HTML structure and codemirror theme

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,35 +3,31 @@ import createTag from './utils/createTag.js';
 //section for Headers and Body
 const requestInfo = createTag({tagName: 'section', className: 'request-info'});
 const requestInfoMainTitle = createTag({tagName: 'h2', className: 'request-info__main-title', tagText: 'Request info'})
-const requestInfoOptions = createTag({tagName: 'ul', className: 'request-info__options',});
-//Headers label
-const requestInfoHeadersTitle = createTag({tagName: 'li', className: 'request-info__headers-title'});
-const headersLink = createTag({tagName: 'a', tagText: 'Headers', tagAttrs: [{key: 'href', value: '#'}]});
-//Body label
-const requestInfoBodyTitle = createTag({tagName: 'li', className: 'request-info__body-title'});
-const bodyLink = createTag({tagName: 'a', tagText: 'Body', tagAttrs: [{key: 'href', value: '#'}]});
+
+const headersButton = createTag({tagName: 'button', className: 'request-info__btn', tagText: 'Headers'});
+const bodyButton = createTag({tagName: 'button', className: 'request-info__btn', tagText: 'Body'});
+
 //Body container and textarea
 const requestInfoBody = createTag({className: 'request-info__body'});
-const requestInfoBodyForm = createTag({tagName: 'form', className: 'request-info__body-form', tagAttrs: [{key: 'method', value: ''}, {key: 'action', value: ''}]}); //do poprawienia
+const requestInfoBodyForm = createTag({tagName: 'form', className: 'request-info__body-form', tagAttrs: [{key: 'method', value: ''}, {key: 'action', value: ''}]});
 const textareaLabel = createTag({tagName: 'label', tagAttrs: [{key: 'for', value: 'textarea'}]});
 const requestInfoBodyTextBox = createTag({tagName: 'textarea', className: 'request-info__body-text-box', tagId: 'textarea'});
 
 
 //Headers and Body section structure
 requestInfo.appendChild(requestInfoMainTitle);
-requestInfo.appendChild(requestInfoOptions);
-
-//Label for Headers and Body
-requestInfoOptions.appendChild(requestInfoHeadersTitle);
-requestInfoHeadersTitle.appendChild(headersLink);
-requestInfoOptions.appendChild(requestInfoBodyTitle);
-requestInfoBodyTitle.appendChild(bodyLink);
+requestInfo.appendChild(headersButton);
+requestInfo.appendChild(bodyButton);
 
 //Body container
 requestInfo.appendChild(requestInfoBody);
 requestInfoBody.appendChild(requestInfoBodyForm);
 requestInfoBodyForm.appendChild(textareaLabel);
-requestInfoBodyForm.appendChild(CodeMirror.fromTextArea(requestInfoBodyTextBox, {lineNumbers: true}));
+requestInfoBodyForm.appendChild(requestInfoBodyTextBox);
 
+CodeMirror.fromTextArea(requestInfoBodyTextBox, {
+    lineNumbers: true,
+    theme: 'yonce',
+})
 
 document.body.appendChild(requestInfo);
