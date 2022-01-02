8948292cbfcb994b76f5cacf239f807b8427536f
Mateusz Zajac <matzaj101@gmail.com>
2022-01-02 22:39:42 +0100

    run prettier on project

--- src/scripts/headers/addHeader.js
+++ src/scripts/headers/addHeader.js
@@ -1,54 +1,59 @@
-import {handleKeyUp, selectItem} from "../utils/headers/autompleteHeaders";
+import { handleKeyUp, selectItem } from "../utils/headers/autompleteHeaders";
 
-const addHeaders=()=> {
-    let rowCounter=0;
-    const form = document.getElementById('headers');
-    // const parentDiv = form.children;
-    const btn =document.getElementById('btn');
-    // const btnForm =document.getElementById('btn-form');
-    const suggestion = document.getElementById('list-container');
-    let lastRow = document.querySelectorAll('.row');
-    const inp  = document.getElementById('first__input');
-    let selectedInput;
-    let inpst = inp;
-    // const Headers =[];
+const addHeaders = () => {
+  let rowCounter = 0;
+  const form = document.getElementById("headers");
+  // const parentDiv = form.children;
+  const btn = document.getElementById("btn");
+  // const btnForm =document.getElementById('btn-form');
+  const suggestion = document.getElementById("list-container");
+  let lastRow = document.querySelectorAll(".row");
+  const inp = document.getElementById("first__input");
+  let selectedInput;
+  let inpst = inp;
+  // const Headers =[];
 
-    btn.addEventListener('click',()=> {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('row');
-        const newInputKey=document.createElement('input');
-        const newInputValue=document.createElement('input');
-        newInputKey.setAttribute('type', 'text');
-        newInputValue.setAttribute('type', 'text');
-        newInputKey.setAttribute('placeholder', 'key');
-        newInputValue.setAttribute('placeholder', 'value');
-        newDiv.appendChild(newInputKey);
-        newDiv.appendChild(newInputValue);
-        form.appendChild(newDiv);
-        rowCounter++;
-        lastRow = document.querySelectorAll('.row')[rowCounter]
+  btn.addEventListener("click", () => {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("row");
+    const newInputKey = document.createElement("input");
+    const newInputValue = document.createElement("input");
+    newInputKey.setAttribute("type", "text");
+    newInputValue.setAttribute("type", "text");
+    newInputKey.setAttribute("placeholder", "key");
+    newInputValue.setAttribute("placeholder", "value");
+    newDiv.appendChild(newInputKey);
+    newDiv.appendChild(newInputValue);
+    form.appendChild(newDiv);
+    rowCounter++;
+    lastRow = document.querySelectorAll(".row")[rowCounter];
 
+    selectedInput = lastRow.children[0];
+    selectedInput.addEventListener("keyup", (event) => {
+      handleKeyUp(event, suggestion);
+    });
+    inpst = selectedInput;
+  });
+  // btnForm.addEventListener('click',()=> {
+  //     for (let i=0; i<parentDiv.length;i++) {
+  //         const inputArr = Array.from(parentDiv[i].children);
+  //         Headers.push(inputArr.map(x=>x.value))
+  //     }
+  //     generateObjHeader(Headers);
+  // })
 
-        selectedInput= lastRow.children[0];
-        selectedInput.addEventListener('keyup',(event)=>{handleKeyUp(event, suggestion)});
-        inpst=selectedInput;
-    })
-    // btnForm.addEventListener('click',()=> {
-    //     for (let i=0; i<parentDiv.length;i++) {
-    //         const inputArr = Array.from(parentDiv[i].children);
-    //         Headers.push(inputArr.map(x=>x.value))
-    //     }
-    //     generateObjHeader(Headers);
-    // })
+  // this function return an object which will be pass to makeRequestAsync as an argument
+  // function generateObjHeader(array){
+  //     const obj = Object.fromEntries(array);
+  //     console.log(obj)
+  // }
 
-    // this function return an object which will be pass to makeRequestAsync as an argument
-    // function generateObjHeader(array){
-    //     const obj = Object.fromEntries(array);
-    //     console.log(obj)
-    // }
+  suggestion.addEventListener("click", (event) => {
+    selectItem(event, inpst, suggestion);
+  });
 
-    suggestion.addEventListener('click',(event)=>{selectItem(event,inpst, suggestion)})
-
-    inpst.addEventListener('keyup',(event)=>{handleKeyUp(event, suggestion)})
-}
+  inpst.addEventListener("keyup", (event) => {
+    handleKeyUp(event, suggestion);
+  });
+};
 export default addHeaders;
